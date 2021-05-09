2c128c86fc716872c60fd1c0953267b972f8d2e5
Julien Coloos <julien.coloos@gmail.com>
2021-05-09 19:53:55 +0200

    Handle setting as code executor script
    Rebuilds Function when setting is changed.

--- native-messaging/extension/src/common/unsafe.js
+++ native-messaging/extension/src/common/unsafe.js
@@ -7,22 +7,38 @@ import * as unsafe from './unsafe.js';
 export class CodeExecutor {
 
   constructor(webext, scriptName, argNames, code) {
-    this.webext = webext;
-    this.scriptName = scriptName;
+    var self = this;
+    self.webext = webext;
+    self.scriptName = scriptName;
     // We will pass useful helpers automatically.
     argNames = argNames.concat(['notif', 'unsafe', 'util', 'webext']);
-    this.argNames = argNames;
+    self.argNames = argNames;
+
+    if ((typeof(code) === 'object') && code.addListener) {
+      // Assume we were given a setting: automatically listen to changes.
+      code.addListener(() => {
+        self.setup(code.value);
+      });
+      self.setup(code.value);
+      return;
+    }
+
+    self.setup(code);
+  }
+
+  setup(code) {
+    delete(this.f);
     if (!code || !code.trim()) return;
     try {
       // Note: using 'Function.call' (or 'apply') instead of 'new Function' so
       // that we don't get warnings from code inspection.
       // And yes, I know I want to 'eval' code here.
-      this.f = Function.call(null, argNames, code);
+      this.f = Function.call(null, this.argNames, code);
     } catch (error) {
-      webext.notify({
+      this.webext.notify({
         title: 'Script code setup failed',
         level: 'error',
-        message: `Script: ${scriptName}`,
+        message: `Script: ${this.scriptName}`,
         error: error
       });
     }
