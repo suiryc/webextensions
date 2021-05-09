a1d05b232db73e46b47e299b193bd902f58ce49c
Julien Coloos <julien.coloos@gmail.com>
2021-05-09 19:59:03 +0200

    Rely on dedicated notifier for code setup/execution errors
    Embeds the script name as source.
    There is no need to add it as notification message.

--- native-messaging/extension/src/common/unsafe.js
+++ native-messaging/extension/src/common/unsafe.js
@@ -35,10 +35,8 @@ export class CodeExecutor {
       // And yes, I know I want to 'eval' code here.
       this.f = Function.call(null, this.argNames, code);
     } catch (error) {
-      this.webext.notify({
+      this.getNotif().error({
         title: 'Script code setup failed',
-        level: 'error',
-        message: `Script: ${this.scriptName}`,
         error: error
       });
     }
@@ -61,10 +59,8 @@ export class CodeExecutor {
       var r = await Promise.resolve(this.f.apply(null, argValues));
       return r || {};
     } catch (error) {
-      this.webext.notify({
+      notif.error({
         title: 'Script code execution failed',
-        level: 'error',
-        message: `Script: ${this.scriptName}`,
         error: error
       });
     }
