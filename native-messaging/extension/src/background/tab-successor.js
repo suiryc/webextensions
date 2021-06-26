suiryc/webextensions

commit e780bbeed358a4b026999179b0e4ffda7df59bf7

Code cleanup and minor refactoring
Have a more consistent usage of undefined, null and '' in the code.
e.g. we usually need to check if a string is defined and non-empty, in
which case 'if (s)' is enough.
The same applies to object instances.

We usually don't have to define object fields as undefined, but can
simply not declare them, delete them when needed, and 'if (obj.field)'
to check truthy fields. In which case we can also 'if ("field" in obj)'
to check if a field is present (even if its value is undefined).

Not 'return'ing anything in a function automatically returns 'undefined'
which can simplify some function code when needing to return an object
field value only when the object exists.

To test numbers, Number.isInteger (integer), Number.isFinite (number)
or Number.isNaN can also be used.

Ensure a tabId is passes as tab successor, which simplifies code.

native-messaging/extension/src/background/tab-successor.js
@@ -54,8 +54,6 @@ export class TabSuccessor {
   constructor(tabsHandler) {
     var self = this;
     self.tabsHandler = tabsHandler;
-    // Currently scheduled checkTabs call.
-    self.scheduledCheckTabs = undefined;
     // List of inactive tabs opened.
     self.inactiveOpenedTabs = {
       // By tab id
@@ -69,15 +67,14 @@ export class TabSuccessor {
 
   async chainTabs(tabs, successor, options) {
     var self = this;
-    if (successor === undefined) {
+    if (!successor) {
       // We need at least two tabs to chain.
       if (tabs.length < 2) return;
-      successor = tabs[0];
+      successor = tabs[0].id;
     } else {
       // We need at least one tab to chain.
-      if (tabs.length < 1) return;
+      if (!tabs.length) return;
     }
-    if ((typeof(successor) == 'object') && ('id' in successor)) successor = successor['id'];
     await browser.tabs.moveInSuccession(tabs.map(tab => tab.id), successor, options);
     self.scheduleCheckTabs();
   }
@@ -110,7 +107,7 @@ export class TabSuccessor {
     // Gather tabs per window.
     for (var tab of tabs) {
       // Leave alone tabs that already have a successor.
-      if (!reset && (tab.successorTabId >= 0)) continue;
+      if (!reset && tab.successorTabId) continue;
       var entry = tabsPerWindow[tab.windowId] || [];
       entry.push(tab);
       tabsPerWindow[tab.windowId] = entry;
@@ -130,7 +127,7 @@ export class TabSuccessor {
   async tabCreated(details) {
     var self = this;
     var tab = details.tab;
-    if (tab.active || !(tab.openerTabId >= 0)) {
+    if (tab.active || !tab.openerTabId) {
       self.scheduleCheckTabs();
       return;
     }
@@ -143,13 +140,13 @@ export class TabSuccessor {
     // In case we fail later, chain them right now from first->last->opener.
     self.inactiveOpenedTabs.byId[tab.id] = tab;
     var opened = self.inactiveOpenedTabs.byOpener[tab.openerTabId] || [];
-    var tab2 = undefined;
-    var options = undefined;
+    var tab2;
+    var options;
     if (opened.length) {
       // We already have a chain first->last->opener.
       // We only need to append+insert our new tab between the previously last
       // tab and the opener.
-      tab2 = opened.slice(-1)[0];
+      tab2 = opened.slice(-1)[0].id;
       options = { append: true, insert: true };
     } else {
       // This is our first opened tab.
@@ -166,7 +163,7 @@ export class TabSuccessor {
   async tabActivated(details) {
     var self = this;
     var openedTab = self.inactiveOpenedTabs.byId[details.tabId];
-    if (openedTab !== undefined) {
+    if (openedTab) {
       // This was an opened (inactive) tab.
       var opened = self.inactiveOpenedTabs.byOpener[openedTab.openerTabId];
       // Forget all tabs opened by the same opener.
@@ -177,20 +174,21 @@ export class TabSuccessor {
       // Link those tabs from activated->first->last->opener
       var index = opened.indexOf(openedTab);
       opened = [openedTab].concat(opened.slice(0, index).reverse()).concat(opened.slice(index + 1));
-      var successor = undefined;
+      var successor;
+      // If the opener tab still exists, use it as successor.
+      // If not, getting it will fail.
       try {
-        successor = await browser.tabs.get(openedTab.openerTabId);
+        successor = (await browser.tabs.get(openedTab.openerTabId)).id;
       } catch(error) {
-        // Happens if opener has been closed in the meantime.
       }
-      if (successor === undefined) {
-        // If opener is not there, fallbacks to the current successor of the
+      if (!successor) {
+        // If opener is not there, fallback to the current successor of the
         // last tab (which supposedly replaces it).
         try {
-          successor = await browser.tabs.get(opened.slice(-1)[0].id);
-          successor = successor.successorTabId;
+          successor = (await browser.tabs.get(opened.slice(-1)[0].id)).successorTabId;
         } catch(error) {
-          // Should not happen.
+          // Should not happen: if this tab was removed, we should have been notified
+          // already, and we don't expect race condition to be possible here.
           console.log('Could not get last tab in chain of inactive opened tabs:', error);
         }
       }
@@ -198,7 +196,7 @@ export class TabSuccessor {
       await self.chainTabs(opened, successor);
       return;
     }
-    if ((details.previousTabId === null) || (details.previousTabId === undefined)) {
+    if (!details.previousTabId) {
       self.scheduleCheckTabs();
       return;
     }
@@ -217,7 +215,7 @@ export class TabSuccessor {
     var self = this;
     var tabId = details.tabId;
     var openedTab = self.inactiveOpenedTabs.byId[tabId];
-    if (openedTab === undefined) {
+    if (!openedTab) {
       self.scheduleCheckTabs();
       return;
     }
@@ -251,13 +249,13 @@ export class TabSuccessor {
   // Useful when many rapid actions trigger more than one scheduling.
   scheduleCheckTabs(delay = 1000) {
     if (!settings.debug.tabs.successor) return;
-    if (this.scheduledCheckTabs !== undefined) clearTimeout(this.scheduledCheckTabs);
+    if (this.scheduledCheckTabs) clearTimeout(this.scheduledCheckTabs);
     this.scheduledCheckTabs = setTimeout(this.checkTabs.bind(this), delay);
   }
 
   // Checks all tabs and logs debugging information.
   async checkTabs() {
-    this.scheduledCheckTabs = undefined;
+    delete(this.scheduledCheckTabs);
     var tabs = await browser.tabs.query({});
     var tabsInfo = {};
     var tabsByWindow = {};
@@ -279,7 +277,6 @@ export class TabSuccessor {
       var tabId = tab.id;
       var info = tabsInfo[tabId];
       var windowInfo = tabsByWindow[tab.windowId] || {
-        active: undefined,
         withSuccessor: {},
         withoutSuccessor: {},
         chains: []
@@ -312,14 +309,14 @@ export class TabSuccessor {
       // Only process tab if it is not already part of an existing chain.
       if (chain.length) {
         // Check any chain that would be the successor of our chain last tab.
-        var chainSuccessor = undefined;
+        var chainSuccessor;
         for (var chainExisting of tabsByWindow[tab.windowId].chains) {
           if (chainExisting[0].id == info.id) {
             chainSuccessor = chainExisting;
             break;
           }
         }
-        if (chainSuccessor !== undefined) {
+        if (chainSuccessor) {
           // Our chain actually precedes another chain we already built.
           // Prepend it.
           chainSuccessor.unshift.apply(chainSuccessor, chain);

native-messaging/app/src/app.js
@@ -38,7 +38,7 @@ async function onMessage(app, msg) {
 // Logs unhandled messages received.
 function unhandledMessage(msg) {
   console.warn('Received unhandled message feature=<%s> kind=<%s> contentSize=%d',
-    msg.feature, msg.kind, msg.content === undefined ? 0 : msg.content.length)
+    msg.feature, msg.kind, (msg.content || '').length)
   return {
     error: 'Message is not handled by native application',
     message: msg
@@ -128,9 +128,8 @@ function dl_save(app, msg) {
     if (done) return;
     done = true;
     // Upon 'exit', code or signal will be null; normalize for easier handling.
-    if (details.code === null) delete details.code;
-    if (details.code === undefined) details.code = 0;
-    if (details.signal === null) delete details.signal;
+    if (!details.code) details.code = 0;
+    if (!details.signal) delete details.signal;
     var response = { };
     // We consider the action failed if either:
     //  - the application wrote on stderr: (belt and suspenders) we actually
@@ -139,9 +138,9 @@ function dl_save(app, msg) {
     //  - application was interrupted by a signal
     //  - an error happened for the child process itself
     if (stderr.data.length) response.error = `Application failed with message=<${stderr.data}>`;
-    else if (details.code != 0) response.error = `Application failed with return code=<${details.code}>`;
-    else if (details.signal !== undefined) response.error = `Application failed with signal=<${details.signal}>`;
-    else if (details.error !== undefined) response.error = `Application failed with error: ${util.formatObject(details.error)}`;
+    else if (details.code) response.error = `Application failed with return code=<${details.code}>`;
+    else if (details.signal) response.error = `Application failed with signal=<${details.signal}>`;
+    else if ('error' in details) response.error = `Application failed with error: ${util.formatObject(details.error)}`;
     else if (stdout.data.length) response.wsPort = Number(stdout.data.toString());
     deferred.resolve(response);
     // While not strictly necessary, cleanup by destroying streams ...

native-messaging/app/src/native-messaging.js
@@ -142,7 +142,7 @@ class NativeSink extends stream.Writable {
         }
       });
       // Do not overwrite pre-existing 'error' field.
-      if (msg.error === undefined) msg.error = error;
+      if (!msg.error) msg.error = error;
       else msg.jsonError = error;
       // Belt and suspenders: in case cleaned message still fails ...
       try {
@@ -228,7 +228,7 @@ class NativeHandler extends stream.Writable {
       r = Promise.reject(error);
     }
     // Don't handle reply if caller don't expect it.
-    if (msg.correlationId === undefined) return;
+    if (!msg.correlationId) return;
     // Embed reply in 'reply' field, or error in 'error' field.
     r.then(v => {
       self.app.postMessage({reply: v, correlationId: msg.correlationId});
@@ -296,9 +296,9 @@ class NativeApplication {
 
   shutdown(code) {
     var self = this;
-    if (self.sink !== undefined) {
+    if (self.sink) {
       // Belt and suspenders: force exit after timeout.
-      if (code !== undefined) process.exitCode = code;
+      if (code) process.exitCode = code;
       setTimeout(() => {
         self.exit();
       }, 10000);

native-messaging/app/src/util.js
@@ -18,11 +18,8 @@ function formatObject(obj, processed, recursiveLoop) {
       : formatObject(child, processed);
   };
 
-  if (typeof(obj) == 'function') {
-    // Only keep the function name (not the code).
-    var name = obj.name;
-    return `function ${name.length ? name : '(anonymous)'}`;
-  }
+  // Only keep function name (not the code).
+  if (typeof(obj) == 'function') return `function ${obj.name || '(anonymous)'}`;
   if (Array.isArray(obj)) {
     // Recursively handle arrays.
     if (recursiveLoop) return `Array(${obj.length})`;
@@ -40,9 +37,7 @@ function formatObject(obj, processed, recursiveLoop) {
   if ((typeof(obj) != 'object') || (obj === null)) return '' + obj;
 
   // Handle errors.
-  if (obj instanceof Error) {
-    return obj.name + ' message=<' + obj.message + '>';
-  }
+  if (obj instanceof Error) return obj.name + ' message=<' + obj.message + '>';
 
   // If object has its own representation, use it. Otherwise get its name and
   // content.
@@ -56,8 +51,8 @@ function formatObject(obj, processed, recursiveLoop) {
     s += `(failed to stringify) ${error}`;
   }
   if (s.startsWith('[object ')) {
-    var s = obj.constructor.name;
-    if (s.length == 0) s = 'Object';
+    s = obj.constructor.name;
+    if (!s) s = 'Object';
     if (recursiveLoop) return s;
     var idx = 0;
     Object.keys(obj).forEach(f => {
@@ -89,7 +84,7 @@ class Deferred {
     // our embedded promise.
     for (var f of ['catch', 'finally', 'then']) {
       // 'finally' implemented in recent browsers only
-      if (this.promise[f] === undefined) continue;
+      if (!this.promise[f]) continue;
       this[f] = this.promise[f].bind(this.promise);
     }
   }

native-messaging/extension/src/background/background.js
@@ -28,7 +28,7 @@ async function onMessage(extension, msg, sender) {
 
     case constants.KIND_TW_SAVE:
       // Protection: we really do expect this message to come from a tab.
-      if (sender.tab === undefined) return unhandledMessage(msg, sender);
+      if (!sender.tab) return unhandledMessage(msg, sender);
       return tw_save(msg);
       break;
 
@@ -143,7 +143,7 @@ function dl_getVideos(msg) {
 // Clears extension messages.
 function ext_clearMessages(msg) {
   var windowId = msg.windowId;
-  if (windowId === undefined) {
+  if (!windowId) {
     applicationMessages = [];
   } else {
     applicationMessages = applicationMessages.filter(details => {
@@ -188,7 +188,7 @@ function unhandledNativeMessage(app, msg) {
 
 // Logs native application log message.
 function app_console(app, msg) {
-  if (msg.error !== undefined) {
+  if (msg.error) {
     // Application actually failed to properly send the log message.
     console.error(`[${app.appId}] Log failure: ${msg.error}`);
     return;
@@ -236,7 +236,7 @@ function addExtensionMessage(details) {
 function updateStatus(windowId) {
   // Update the requested window, or update all known windows.
   var obj = {};
-  if (windowId !== undefined) {
+  if (windowId) {
     obj[windowId] = videosSources[windowId] || [];
   } else {
     obj = videosSources;
@@ -254,8 +254,8 @@ function updateStatus(windowId) {
     var tabHandler = tabsHandler.getActiveTab(windowId);
     var tabId = tabHandler ? tabHandler.id : -1;
     for (var details of applicationMessages) {
-      if ((details.windowId !== undefined) && (details.windowId != windowId)) continue;
-      if ((details.tabId !== undefined) && (details.tabId != tabId)) continue;
+      if (details.windowId && (details.windowId != windowId)) continue;
+      if (details.tabId && (details.tabId != tabId)) continue;
       if (details.level == 'error') {
         hasMessages = '!';
         badgeBackgroundColor = 'red';

native-messaging/extension/src/background/content-scripts.js
@@ -113,7 +113,7 @@ export class ContentScriptHandler {
     // We want the DOM content to be loaded.
     // If frameHandler is not (yet) known, we expect a 'frameAdded' to be
     // triggered soon.
-    if (!details.domLoaded || (frameHandler === undefined)) return;
+    if (!details.domLoaded || !frameHandler) return;
     this.handleFrame(frameHandler);
   }
 

native-messaging/extension/src/background/downloads.js
@@ -14,7 +14,7 @@ const pageContentExtensions = new Set(['htm', 'html', 'php', 'asp', 'aspx', 'asx
 // content we may download).
 // Caller is expected to have excluded what is considered as explicit downloads:
 // attachment or content with filename.
-// Returns the reason it may be a page content (for debugging), or undefined.
+// Returns the reason it may be a page content (for debugging), or falsy value.
 function maybePageContent(url) {
   // We want to know whether the URL *may* just point to a page content (and not
   // a real content we would download). This includes standard pages with static
@@ -26,7 +26,7 @@ function maybePageContent(url) {
   // If it's empty (that is the URL ends with a slash '/'), we assume the URL is
   // *not* for a content to download; most likely it's the 'index' page of the
   // site or one of its paths.
-  if (pathname.length == 0) return 'empty leaf path';
+  if (!pathname) return 'empty leaf path';
   // Otherwise we get the 'extension' (considering path as a filename).
   var split = pathname.split(/\./);
   var extension = (split.length > 1) ? split.pop().toLowerCase() : '';
@@ -36,11 +36,10 @@ function maybePageContent(url) {
   //  - the extension length is too big to be considered a real extension: there
   //    there are many valid 4-letters extensions (webm/webp etc.), almost none
   //    with 5-letters, so set the limit accordingly
-  if (extension.length == 0) return 'path has no file extension';
+  if (!extension) return 'path has no file extension';
   if (extension.length > 5) return 'path does not appear to be a file with extension';
   // Finally, take into account known static/dynamic page extensions.
   if (pageContentExtensions.has(extension)) return 'path matches known page extensions';
-  return undefined;
 }
 
 class DlMngrClient {
@@ -62,30 +61,29 @@ class DlMngrClient {
       }, settings.notifyTtl);
     }
 
-    // Drop undefined (or null) fields.
     util.cleanupFields(details);
     util.cleanupFields(params);
 
     // Set 'kind' field, in case we pass this message to the native app.
     details.kind = constants.KIND_DOWNLOAD;
     // Fill requested fields.
-    if (params.addCookie && (details.cookie === undefined)) {
+    if (params.addCookie && !details.cookie) {
       try {
         details.cookie = await http.getCookie(details.url);
       } catch (error) {
         console.log('Could not add cookie for url=<%s>: %o', details.url, error);
       }
     }
-    if (params.addUserAgent && (details.userAgent === undefined)) {
+    if (params.addUserAgent && !details.userAgent) {
       details.userAgent = navigator.userAgent;
     }
-    if (params.addComment && (details.comment === undefined)) {
+    if (params.addComment && !details.comment) {
       var comment = [];
-      if (details.file !== undefined) comment.push(`Download filename: ${details.file}`);
-      if (params.mimeFilename !== undefined) comment.push(`MIME filename: ${params.mimeFilename}`);
+      if (details.file) comment.push(`Download filename: ${details.file}`);
+      if (params.mimeFilename) comment.push(`MIME filename: ${params.mimeFilename}`);
       comment.push(`URL filename: ${util.getFilename(details.url)}`);
-      if (params.tabTitle !== undefined) comment.push(`Page title: ${params.tabTitle}`);
-      if ((params.linkText !== undefined) && (params.linkText != details.url)) comment.push(`Link text: ${params.linkText}`);
+      if (params.tabTitle) comment.push(`Page title: ${params.tabTitle}`);
+      if (params.linkText && (params.linkText != details.url)) comment.push(`Link text: ${params.linkText}`);
       details.comment = comment.join('\n');
     }
 
@@ -133,9 +131,9 @@ class DlMngrClient {
       // If WebSocket returns non-0 code, log it. It is useless to fallback to
       // the native application, since the same should happen (except we don't
       // wait for its return code). Better return a proper error.
-      if (r.code !== 0) {
+      if (r.code) {
         // Note: error will be notified and logged.
-        r.error = (r.output !== undefined) ? r.output : 'WebSocket returned non-0 response code';
+        r.error = r.output || 'WebSocket returned non-0 response code';
       }
       return handleError(r);
     } catch (error) {
@@ -234,7 +232,7 @@ export class RequestsHandler {
     // If not done, setup our (bound to us) callbacks (used as listeners).
     // Note: we need those callbacks to remain the same so that we can remove
     // any listener that was previously added.
-    if (this.listeners === undefined) {
+    if (!this.listeners) {
       this.listeners = {};
       ['onRequest', 'onResponse', 'onRequestCompleted', 'onRequestError', 'onDownload'].forEach(key => {
         this.listeners[key] = this[key].bind(this);
@@ -300,14 +298,13 @@ export class RequestsHandler {
 
   ignoreNext(ttl) {
     var self = this;
-    if (ttl === undefined) ttl = constants.IGNORE_NEXT_TTL;
-    // Cancel if requested (non-positive TTL).
-    if (ttl <= 0) {
+    // Cancel if requested.
+    if (!ttl) {
       self.cancelIgnoreNext();
       return;
     }
     // Nothing to do if we already are ignoring.
-    if (self.ignoringNext !== undefined) return;
+    if (self.ignoringNext) return;
     console.log('Ignoring next interception with ttl=<%s>', ttl);
     var ttlStep = 1000;
     // Start TTL ('+ ttlStep' to reuse the function to decrement the TTL).
@@ -316,7 +313,7 @@ export class RequestsHandler {
     };
     function decrement() {
       // Stop if we are not ignoring anymore.
-      if (self.ignoringNext === undefined) return;
+      if (!self.ignoringNext) return;
       // Decrement and cancel if TTL is reached.
       self.ignoringNext.ttl -= ttlStep;
       if (self.ignoringNext.ttl <= 0) {
@@ -337,8 +334,8 @@ export class RequestsHandler {
 
   cancelIgnoreNext() {
     // Nothing to do it we are not ignoring.
-    if (this.ignoringNext === undefined) return;
-    if (this.ignoringNext.timeout !== undefined) clearTimeout(this.ignoringNext.timeout);
+    if (!this.ignoringNext) return;
+    if (this.ignoringNext.timeout) clearTimeout(this.ignoringNext.timeout);
     this.webext.sendMessage({
       target: constants.TARGET_BROWSER_ACTION,
       kind: constants.KIND_DL_IGNORE_NEXT,
@@ -348,13 +345,13 @@ export class RequestsHandler {
   }
 
   checkIgnoreNext() {
-    if (this.ignoringNext === undefined) return false;
+    if (!this.ignoringNext) return false;
     this.cancelIgnoreNext();
     return true;
   }
 
   addRequestDetails(base, requestDetails) {
-    if ((requestDetails === undefined) || (requestDetails === null)) return;
+    if (!requestDetails) return;
     var key = requestDetails.url;
     var entries = base[key] || [];
     requestDetails.timestamp = util.getTimestamp();
@@ -364,9 +361,9 @@ export class RequestsHandler {
 
   removeRequestdetails(base, key) {
     var entries = base[key];
-    if (entries === undefined) return;
+    if (!entries) return;
     var removed = entries.shift();
-    if (entries.length == 0) delete(base[key]);
+    if (!entries.length) delete(base[key]);
     else base[key] = entries;
     return removed;
   }
@@ -383,7 +380,7 @@ export class RequestsHandler {
 
   cleanupUnintercepted() {
     for (var [key, unintercepted] of Object.entries(this.unintercepted)) {
-      while ((unintercepted.length > 0) && (util.getTimestamp() - unintercepted[0].timestamp > constants.REQUESTS_TTL)) {
+      while (unintercepted.length && (util.getTimestamp() - unintercepted[0].timestamp > constants.REQUESTS_TTL)) {
         console.warn('Dropping incomplete unintercepted download %o: TTL reached', unintercepted[0]);
         this.removeUnintercepted(key);
         // Note: we share the array with removeUnintercepted.
@@ -403,7 +400,7 @@ export class RequestsHandler {
 
   cleanupCompletedRequests() {
     for (var [key, completed] of Object.entries(this.requestsCompleted)) {
-      while ((completed.length > 0) && (util.getTimestamp() - completed[0].timestamp > constants.REQUESTS_TTL)) {
+      while (completed.length && (util.getTimestamp() - completed[0].timestamp > constants.REQUESTS_TTL)) {
         if (settings.debug.downloads) console.log('Dropping completed request %o: TTL reached', completed[0]);
         this.removeCompletedRequest(key);
         // Note: we share the array with removeCompletedRequest.
@@ -496,10 +493,7 @@ export class RequestsHandler {
       tabTitle: tabTitle
     });
     // Cancel the request if we successfully managed to trigger the download.
-    if (r.error) return {};
-    return {
-      cancel: true
-    };
+    return {cancel: !r.error};
   }
 
   onDownload(download) {
@@ -520,7 +514,7 @@ export class RequestsHandler {
       return [download];
     }).then(r => {
       var d = r.find(d => d.id == download.id);
-      download = (d === undefined) ? download : d;
+      if (d) download = d;
 
       // Don't process completed downloads.
       // Note: does not appear to ever happen even if download has really been
@@ -528,13 +522,13 @@ export class RequestsHandler {
       if (download.state === 'complete') return self.manageDownload(download, false, 'Download completed');
 
       // Don't process failed downloads.
-      if ((download.error !== undefined) && (download.error !== null)) return self.manageDownload(download, false, 'Download failed');
+      if (download.error) return self.manageDownload(download, false, 'Download failed');
 
       // Do not intercept if corresponding request would not have been.
 
       // Don't intercept 'too small' download
-      var totalBytes = (download.totalBytes === undefined) ? -1 : download.totalBytes;
-      var bytesReceived = ((totalBytes <= 0) || (download.bytesReceived === undefined)) ? 0 : download.bytesReceived;
+      var totalBytes = Number.isInteger(download.totalBytes) ? download.totalBytes : -1;
+      var bytesReceived = ((totalBytes > 0) && Number.isInteger(download.bytesReceived)) ? download.bytesReceived : 0;
       var remaining = totalBytes - bytesReceived;
       if ((remaining >= 0) && (remaining < settings.interceptSize)) return self.manageDownload(download, false, `Remaining length=<${remaining}> below limit`);
 
@@ -557,7 +551,7 @@ export class RequestsHandler {
           // is being downloaded/displayed, we don't want to intercept. The user
           // can still close the tab and instead 'Download' the original target
           // link to trigger interception intead of opening the file in a tab.
-          return (tabs.length > 0);
+          return !!tabs.length;
         }).catch(error => {
           console.warn('Cannot determine whether download %o is displayed: %o', download, error);
           return false;
@@ -666,7 +660,7 @@ export class RequestsHandler {
     // on this kind of HTML element, instead of a plain link. In this case
     // srcUrl is the 'src' value of the element (while linkUrl is the URL target
     // link otherwise).
-    var url = (info.mediaType !== undefined) ? info.srcUrl : info.linkUrl;
+    var url = info.mediaType ? info.srcUrl : info.linkUrl;
 
     if (!http.canDownload(url)) {
       if (settings.notifyDownload) {
@@ -688,12 +682,9 @@ export class RequestsHandler {
     }
 
     // Determine the referrer: either the frame or the page.
-    var referrer = info.frameUrl;
-    if (referrer === undefined) referrer = info.pageUrl;
-
     return await dlMngr.download({
       url: url,
-      referrer: referrer
+      referrer: info.frameUrl || info.pageUrl
     }, {
       addCookie: true,
       addUserAgent: true,
@@ -750,7 +741,7 @@ class RequestDetails extends http.RequestDetails {
 
     // Find the corresponding request.
     this.sent = handler.requests[response.requestId];
-    if (this.sent === undefined) return handler.manageRequest(this, false, 'No matching request');
+    if (!this.sent) return handler.manageRequest(this, false, 'No matching request');
     delete(handler.requests[response.requestId]);
 
     // Special case (Firefox):
@@ -759,7 +750,7 @@ class RequestDetails extends http.RequestDetails {
     //  - ip is not present (Firefox 56) or null (Firefox 62)
     // We don't want/need to intercept download if data has already been fetched
     // by the browser.
-    var fromCache = (response.fromCache === true) || (!('ip' in response) || (response.ip === null) || (response.ip === undefined));
+    var fromCache = response.fromCache || !('ip' in response) || !response.ip;
     if (fromCache) return handler.manageRequest(this, false, 'Response cached');
 
     // Only process standard success code. This filters out errors, redirects,
@@ -775,7 +766,7 @@ class RequestDetails extends http.RequestDetails {
     // In this case, still check if we would intercept from the first response
     // (indicates remaining size). As a side effect the second response will not
     // be intercepted due to missing matching request (we consume it here).
-    if ((statusCode == 206) && (http.findHeader(this.sent.requestHeaders, 'Range') !== undefined)) return handler.manageRequest(this, false, 'Skip partial content request');
+    if ((statusCode == 206) && http.findHeader(this.sent.requestHeaders, 'Range')) return handler.manageRequest(this, false, 'Skip partial content request');
 
     // Parse response to get content length, type, disposition.
     this.parseResponse(settings.interceptSize);
@@ -810,7 +801,7 @@ class RequestDetails extends http.RequestDetails {
     //  - URL path is compatible with a page content (name/extension)
     if (!this.hasFilename()) {
       var reason = maybePageContent(this.url);
-      if (reason !== undefined) return handler.manageRequest(this, false, `Maybe page content; ${reason}`);
+      if (reason) return handler.manageRequest(this, false, `Maybe page content; ${reason}`);
     }
 
     // Intercept everything else. Size is unknown, but it's not supposed to be

commit 572e13d83ef44d6864e82824e03c34771893595d

Fix tab successor chain initialization
Tab.successorTabId is -1 when there is no successor. So unlike
other tab id testing (id is not supposed to negative), do check
the value is positive.
Bug introduced in e780bbeed358a4b026999179b0e4ffda7df59bf7.

native-messaging/extension/src/background/tab-successor.js
@@ -123,7 +123,7 @@ export class TabSuccessor {
     // Gather tabs per window.
     for (var tab of tabs) {
       // Leave alone tabs that already have a successor.
-      if (!reset && tab.successorTabId) continue;
+      if (!reset && (tab.successorTabId > 0)) continue;
       var entry = tabsByWindow[tab.windowId] || [];
       entry.push(tab);
       tabsByWindow[tab.windowId] = entry;