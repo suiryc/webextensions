0b79be6bfc72a8790701aa671ff42661d06ce22b
Julien Coloos <julien.coloos@gmail.com>
2024-10-27 21:15:25 +0100

    ff: also ignore text content types for video sources

--- native-messaging/extension/src/background/video-sources.js
+++ native-messaging/extension/src/background/video-sources.js
@@ -9,6 +9,7 @@ import { settings } from '../common/settings.js';
 
 
 function checkVideoContentType(contentType) {
+  if (contentType.isText()) return 'Text';
   if (contentType.isImage()) return 'Image';
   if (contentType.isAudio()) return 'Audio';
 }
